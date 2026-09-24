7065c16fb53facd94a5de81362cb27cfd9b2ea9c
lynelk <86437305+lynelk@users.noreply.github.com>
2026-09-24 10:28:48 +0300

    brand: serve v3 vector monogram on web

--- apps/web/src/components/OpFinSymbol.tsx
+++ apps/web/src/components/OpFinSymbol.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-/** The supplied monogram; surrounding app-name text is an interface label. */
+/** OpFin Brand System v3 monogram. Surrounding app-name text is an interface label. */
 export function OpFinSymbol({ reverse = false }: { reverse?: boolean }) {
   return <Image
-    src={reverse ? "/brand/opfin-symbol-reverse.png" : "/brand/opfin-symbol.png"}
+    src={reverse ? "/brand/opfin-symbol-reverse.svg" : "/brand/opfin-symbol.svg"}
     alt=""
     aria-hidden="true"
     width={38}
